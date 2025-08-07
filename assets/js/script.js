4225b3c761b842a21ff11405d6cced12143b4605
Kobe D. Estabalaya <kdestabalaya@sscgi.com>
2025-08-07 11:56:59 +0800

    August 7 2025 Afternoon

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,31 +9,3 @@ function toggleDarkMode() {
       : "images/arts/kobe.png";
   }
 
-// revolving galery
-
-const images = document.querySelectorAll('.carousel-images img');
-let currentIndex = 0;
-
-function showImage(index) {
-  images.forEach((img, i) => {
-    img.classList.toggle('active', i === index);
-  });
-}
-
-document.querySelector('.prev').addEventListener('click', () => {
-  currentIndex = (currentIndex - 1 + images.length) % images.length;
-  showImage(currentIndex);
-});
-
-document.querySelector('.next').addEventListener('click', () => {
-  currentIndex = (currentIndex + 1) % images.length;
-  showImage(currentIndex);
-});
-
-// Auto-rotate every 3 seconds (optional)
-setInterval(() => {
-  currentIndex = (currentIndex + 1) % images.length;
-  showImage(currentIndex);
-}, 3000);
-
-
